06c88efd81c329aa145f945874f504c823d59045
Alexey Rumyntsev <lean_back@mail.ru>
2022-12-03 00:44:41 +0300

    third commit. Added allure report

--- src/main/java/site/stellarburgers/user/UserGenerator.java
+++ src/main/java/site/stellarburgers/user/UserGenerator.java
@@ -3,6 +3,6 @@ package site.stellarburgers.user;
 public class UserGenerator {
 
     public static User getDefaultUser() {
-        return new User ("Nooges123@mail.ru", "test123", "Nooges123Test");
+        return new User ("myFinishTest@mail.ru", "finishTest", "GoodBye");
     }
 }
